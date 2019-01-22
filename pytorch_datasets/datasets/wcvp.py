e935983a24ec5b48ab1bc15823d87bff105d1dfa
mpeven <mpeven@gmail.com>
2019-01-22 12:46:38 -0500

    Update car datasets to use core DataSet class

--- pytorch_datasets/datasets/wcvp.py
+++ pytorch_datasets/datasets/wcvp.py
@@ -1,35 +1,19 @@
-import os
 import glob
 from tqdm import tqdm
-from PIL import Image
 import numpy as np
-import torch
+from pytorch_datasets.dataset import DataSet
 
 
-class WCVP(torch.utils.data.Dataset):
-    dset_location = '/hdd/Datasets/WCVP/'
-
-    def __init__(self):
-        self.dataset = self.create_dataset()
-
-    def create_dataset(self):
-        dataset = []
-        for annotation_file in tqdm(glob.glob(self.dset_location + "*/*.txt"),
+class WCVP(DataSet):
+    def __init__(self, root):
+        self.dataset = []
+        for annotation_file in tqdm(glob.glob(root + "*/*.txt"),
                                     ncols=100,
                                     desc="Creating WCVP dataset"
                                     ):
             xyz = open(annotation_file, "r").readlines()[3].split()
-            dataset.append({
+            self.dataset.append({
                 'image_file': annotation_file.replace('txt', 'jpg'),
                 'orientation': np.arctan2(float(xyz[0]), float(xyz[1])) * 180. / np.pi,
                 'source': 'WCVP',
             })
-        return dataset
-
-    def __len__(self):
-        return len(self.dataset)
-
-    def __getitem__(self, idx):
-        datum = self.dataset[idx]
-        datum['image'] = Image.open(datum['image_file']).convert('RGB')
-        return datum
