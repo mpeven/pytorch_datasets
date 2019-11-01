6a1709bfb59739b5e7ce299c70350b0080209c82
mpeven <mpeven@gmail.com>
2019-10-31 21:17:25 -0400

    Fix a couple errors

--- pytorch_datasets/datasets/epfl.py
+++ pytorch_datasets/datasets/epfl.py
@@ -1,3 +1,4 @@
+import os
 import datetime
 from tqdm import tqdm
 import pandas as pd
@@ -5,17 +6,18 @@ from pytorch_datasets.dataset import DataSet
 
 
 class EPFL(DataSet):
-    def __init__(self, root):
+    def __init__(self, root, transforms=None):
+        super().__init__(transforms)
         self.dataset = []
-        seq_info = open(root + "tripod-seq.txt", "r").read().split('\n')
+        seq_info = open(os.path.join(root, "tripod-seq.txt"), "r").read().split('\n')
         df = pd.DataFrame({
             'total_frames': [int(x) for x in seq_info[1].split()],
             'rotation_frames': [int(x) for x in seq_info[4].split()],
             'front_frame': [int(x) for x in seq_info[5].split()],
             'rotation_dir': [int(x) for x in seq_info[6].split()],
         })
-        for seq_idx, row in tqdm(df.iterrows(), ncols=115, desc="Getting EPFL data", total=len(df)):
-            times = open(root + "times_{:02d}.txt".format(seq_idx + 1), "r").read().split('\n')
+        for seq_idx, row in df.iterrows():
+            times = open(os.path.join(root, "times_{:02d}.txt".format(seq_idx + 1)), "r").read().split('\n')
             times = [datetime.datetime.strptime(x, '%Y:%m:%d %H:%M:%S') for x in times[:-1]]
             total_rotation_time = (times[row['rotation_frames'] - 1] - times[0]).total_seconds()
             front_degree_fraction = (times[row['front_frame'] - 1] - times[0]).total_seconds() / total_rotation_time
@@ -23,9 +25,9 @@ class EPFL(DataSet):
             for frame in range(0, row['rotation_frames']):
                 fraction_through_rotation = (times[frame] - times[0]).total_seconds() / total_rotation_time
                 current_orientation = -90 - (-1 * row['rotation_dir'] * (front_degree_fraction - fraction_through_rotation) * 360)
-
+                current_orientation = int(360 + current_orientation) % 360
                 self.dataset.append({
-                    'image_file': root + 'tripod_seq_{:02d}_{:03d}.jpg'.format(seq_idx + 1, frame + 1),
+                    'image_file': os.path.join(root, 'tripod_seq_{:02d}_{:03d}.jpg'.format(seq_idx + 1, frame + 1)),
                     'source': 'EPFL',
                     'orientation': current_orientation,
                 })
--- pytorch_datasets/datasets/wcvp.py
+++ pytorch_datasets/datasets/wcvp.py
@@ -1,19 +1,18 @@
 import glob
+import os
 from tqdm import tqdm
 import numpy as np
 from pytorch_datasets.dataset import DataSet
 
 
 class WCVP(DataSet):
-    def __init__(self, root):
+    def __init__(self, root, transforms=None):
+        super().__init__(transforms)
         self.dataset = []
-        for annotation_file in tqdm(glob.glob(root + "*/*.txt"),
-                                    ncols=100,
-                                    desc="Creating WCVP dataset"
-                                    ):
+        for annotation_file in glob.glob(os.path.join(root, "*/*.txt")):
             xyz = open(annotation_file, "r").readlines()[3].split()
             self.dataset.append({
                 'image_file': annotation_file.replace('txt', 'jpg'),
-                'orientation': np.arctan2(float(xyz[0]), float(xyz[1])) * 180. / np.pi,
+                'orientation': (int(np.arctan2(float(xyz[0]), float(xyz[1])) * 180. / np.pi) + 360) % 360,
                 'source': 'WCVP',
             })
